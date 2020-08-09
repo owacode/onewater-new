83ba5cb230a4f172ea726a2aee3455173b2b8348
mandeepb29 <mandeepb29@gmail.com>
2020-08-09 19:22:49 +0530

    august build -1

--- src/app/blog-website/blog-website-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule, OnInit } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-@NgModule({
-  imports: [],
-  exports: [RouterModule]
-})
-export class BlogWebsiteRoutingModule{ 
-}
