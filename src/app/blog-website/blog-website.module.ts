ce87b88f917a60de386149b6b535f3e268cc35a6
mandeepb29 <mandeepb29@gmail.com>
2020-11-05 22:13:03 +0530

    deploy bug fix 3

--- src/app/blog-website/blog-website.module.ts
+++ src/app/blog-website/blog-website.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BlogWebsiteRoutingModule } from './blog-website-routing.module';
+import { BlogWebsiteComponent } from './blog-website.component';
 
 @NgModule({
   declarations: [
+    BlogWebsiteComponent
     ],
   imports: [
-    CommonModule,
-    BlogWebsiteRoutingModule
+    CommonModule
   ]
 })
 
