63222367ad854a722477732381f19e33843f70de
mandeepb29 <mandeepb29@gmail.com>
2020-11-12 01:09:40 +0530

    blog updated

--- src/app/onewaterblog/onewaterblog-category/onewaterblog-category.component.ts
+++ src/app/onewaterblog/onewaterblog-category/onewaterblog-category.component.ts
@@ -1,49 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { BlogService } from 'src/app/services/blog.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { BlogService } from "src/app/services/blog.service";
 
 @Component({
-  selector: 'app-onewaterblog-category',
-  templateUrl: './onewaterblog-category.component.html',
-  styleUrls: ['./onewaterblog-category.component.scss']
+  selector: "app-onewaterblog-category",
+  templateUrl: "./onewaterblog-category.component.html",
+  styleUrls: ["./onewaterblog-category.component.scss"],
 })
 export class OnewaterblogCategoryComponent implements OnInit {
+  constructor(public blogService: BlogService, public route: ActivatedRoute) {}
+  catID;
+  catName;
+  categoryBlogs = [];
 
-  constructor(public blogService: BlogService, public route: ActivatedRoute) { }
-
-  categoryBlogs = []
-
-  categories = []
+  categories = [];
 
   ngOnInit() {
-    this.route.params.subscribe(res => {
-      this.blogService.getBlogsForCategory(res.id)
-      .then((res: any) => {
-        console.log(res)
-        this.categoryBlogs = res
-      })
-    })
-    this.blogService.getBlogCategories()
-.then((res: any) => {
-  res.forEach(element => {
-    if(element.name == "Innovation and Research")
-    this.categories.push({ name: "Innovation and Research", id: element.id, img: 'assets/img/icons/research.svg'})
-    else if(element.name == "Legislative and Regulatory")
-    this.categories.push({ name: "Legislative and Regulatory", id: element.id, img: 'assets/img/icons/regulatory.svg'})
-    else if(element.name == "Management &amp; Finance")
-    this.categories.push({ name: "Management &amp; Finance", id: element.id, img: 'assets/img/icons/finance.svg'})
-    else if(element.name == "Storm Water")
-    this.categories.push({ name: "Storm Water", id: element.id, img: 'assets/img/icons/storm.svg'})
-    else if(element.name == "Sustainable Development")
-    this.categories.push({ name: "Sustainable Development", id: element.id, img: 'assets/img/icons/sustainable.svg'})
-    else if(element.name == "Waste Water")
-    this.categories.push({ name: "Waste Water", id: element.id, img: 'assets/img/icons/waste-water.svg'})
-    else if(element.name == "Water")
-    this.categories.push({ name: "Water", id: element.id, img: 'assets/img/icons/water.svg'})
-    else
-    this.categories.push({ name: element.name, id: element.id, img: 'assets/img/icons/default-category.svg'})
-  });
-})
+    this.route.params.subscribe((res) => {
+      this.catID = res.id;
+      this.blogService.getBlogsForCategory(res.id).then((res: any) => {
+        console.log(res);
+        this.categoryBlogs = res;
+      });
+
+      this.getCategories();
+    });
   }
 
+  getCategories(){
+    this.categories = [];
+    this.blogService.getBlogCategories().then((res: any) => {
+      res.forEach((element) => {
+        if (element.name == "Innovation and Research")
+          this.categories.push({
+            name: "Innovation and Research",
+            id: element.id,
+            img: "assets/img/icons/research.svg",
+          });
+        else if (element.name == "Legislative and Regulatory")
+          this.categories.push({
+            name: "Legislative and Regulatory",
+            id: element.id,
+            img: "assets/img/icons/regulatory.svg",
+          });
+        else if (element.name == "Management &amp; Finance")
+          this.categories.push({
+            name: "Management &amp; Finance",
+            id: element.id,
+            img: "assets/img/icons/finance.svg",
+          });
+        else if (element.name == "Storm Water")
+          this.categories.push({
+            name: "Storm Water",
+            id: element.id,
+            img: "assets/img/icons/storm.svg",
+          });
+        else if (element.name == "Sustainable Development")
+          this.categories.push({
+            name: "Sustainable Development",
+            id: element.id,
+            img: "assets/img/icons/sustainable.svg",
+          });
+        else if (element.name == "Waste Water")
+          this.categories.push({
+            name: "Waste Water",
+            id: element.id,
+            img: "assets/img/icons/waste-water.svg",
+          });
+        else if (element.name == "Water")
+          this.categories.push({
+            name: "Water",
+            id: element.id,
+            img: "assets/img/icons/water.svg",
+          });
+        else
+          this.categories.push({
+            name: element.name,
+            id: element.id,
+            img: "assets/img/icons/default-category.svg",
+          });
+      });
+
+      this.categories.forEach((el) => {
+        if (el.id == this.catID) {
+          this.catName = el.name;
+        }
+      });
+    });
+  }
 }
