97588cb444be8adecfd607e067d70d8723be56f4
Mohit Patel <mohitpatel9753@gmail.com>
2020-11-11 13:15:53 +0530

    backend url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendURL: 'http://localhost:3000/api'
+  backendURL: 'https://onewater-backend.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendURL: 'http://localhost:3000/api'
+  backendURL: 'https://onewater-backend.herokuapp.com/api'
 };
 
 /*
